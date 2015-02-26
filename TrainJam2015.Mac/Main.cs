2ea2f8b08f5f6dc7e67c2272ad0230635aaf7f5e
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-02-26 16:31:16 -0600

    Initial commit

---
+++ TrainJam2015.Mac/Main.cs
@@ -0,0 +1,32 @@
+﻿using MonoMac.AppKit;
+using CocosSharp;
+
+
+namespace TrainJam2015.Mac
+{
+	class Program : NSApplicationDelegate
+	{
+		static void Main (string[] args)
+		{
+			NSApplication.Init ();
+
+			var application = NSApplication.SharedApplication;
+			application.Delegate = new Program ();
+			application.Run ();
+		}
+
+		public override void DidFinishLaunching (MonoMac.Foundation.NSNotification notification)
+		{
+			var application = new CCApplication (false, new CCSize (1024f, 768f)) {
+				ApplicationDelegate = new AppDelegate ()
+			};
+			application.StartGame();
+		}
+
+		public override bool ApplicationShouldTerminateAfterLastWindowClosed (NSApplication sender)
+		{
+			return true;
+		}
+	}
+}
+
