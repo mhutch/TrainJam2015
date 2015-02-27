dbe288fe885d12a9ed784e86fc5705e8cf377c84
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-02-26 18:56:37 -0600

    Basic particle and chamber classes

---
+++ TrainJam2015/CloudChamber.cs
@@ -0,0 +1,76 @@
+﻿//
+// CloudChamber.cs
+//
+// Author:
+//       Michael Hutchinson <m.j.hutchinson@gmail.com>
+//
+// Copyright (c) 2015 Michael Hutchinson
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using CocosSharp;
+using Box2D.Dynamics;
+using Box2D.Common;
+
+namespace TrainJam2015
+{
+	public class CloudChamber : CCLayer
+	{
+		b2World world;
+
+		CCSize screenSize;
+
+		public CloudChamber (CCSize size) : base (size)
+		{
+			screenSize = size;
+		}
+
+		protected override void AddedToScene ()
+		{
+			base.AddedToScene ();
+
+			world = new b2World (b2Vec2.Zero) {
+				AllowSleep = false,
+			};
+			world.SetContinuousPhysics (true);
+
+			Color = new CCColor3B (220, 230, 255);
+
+			var p = new Particle (world, new CCPoint (screenSize.Center));
+			AddChild (p);
+
+			//Schedule (Tick);
+		}
+
+		void Tick(float dt)
+		{
+			world.Step (dt, 8, 1);
+
+			for (var b = world.BodyList; b != null; b = b.Next) {
+				if (b.UserData != null) {
+					var node = ((CCNode)b.UserData);
+					node.Position = new CCPoint (b.Position.x, b.Position.y);
+					node.Rotation = -1 * CCMacros.CCRadiansToDegrees(b.Angle);
+				}
+			}
+
+		}
+	}
+}
+
---
+++ TrainJam2015/Particle.cs
@@ -0,0 +1,56 @@
+﻿//
+// Particle.cs
+//
+// Author:
+//       Michael Hutchinson <m.j.hutchinson@gmail.com>
+//
+// Copyright (c) 2015 Michael Hutchinson
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using CocosSharp;
+using Box2D.Dynamics;
+
+namespace TrainJam2015
+{
+	public class Particle : CCNode
+	{
+		b2World world;
+		b2Body body;
+		CCSprite sprite;
+
+		public Particle (b2World world, CCPoint position)
+		{
+			Position = position;
+			this.world = world;
+
+			sprite = new CCSprite ("Particle");
+			AddChild (sprite);
+
+			var bodyDef = new b2BodyDef ();
+			bodyDef.type = b2BodyType.b2_staticBody;
+			bodyDef.position.x = position.X;
+			bodyDef.position.y = position.Y;
+
+			body = world.CreateBody (bodyDef);
+			body.UserData = this;
+		}
+	}
+}
+
